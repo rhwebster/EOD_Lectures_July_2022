674efe642bafb9b87eeaa763a6dca4b6eaba45ce
Ryan Webster <ryan.h.webster@gmail.com>
2022-07-06 16:51:50 -0600

    day3

---
+++ Wk1/day3.js
@@ -0,0 +1,7 @@
+// fizzBuzz (for loop && while loop)
+
+/* 
+Define a function fizzBuzz(max) that takes a number (max) and prints every number 
+from 0 to max (not inclusive) that is divisible by either 3 or 5, but not both.
+*/
+
