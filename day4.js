b31c828009b5fc2a96a66d6f1e9aee4ea8f1e944
Ryan Webster <ryan.h.webster@gmail.com>
2022-07-07 17:10:11 -0600

    string changer

--- day4.js
+++ day4.js
@@ -8,3 +8,15 @@ Then, write a function mostExpensiveGroceries(groceriesList) that takes in a 2-d
 array of grocery items and returns the index of the sub-array with the highest cost.
 */
 
+
+//StringChanger
+/*
+Build a function called stringChanger() that takes in two arguments: a word and 
+an operation. Based on the operation, your function will return the word modified 
+in some way. The operations are:
+'capitalize': Capitalize the first letter in the word.
+'uppercase': Capitalize every letter in the word.
+'double': Return the word twice in a row.
+'reverse': Return the string with the letters in reverse order.
+If the operation is invalid, return the word, unchanged.
+*/
