772f72787440e94ae8cf1f182f763e448ffa97dc
Ryan Webster <ryan.h.webster@gmail.com>
2022-07-12 13:37:23 -0700

    day2

--- Wk1/paproblem.js
+++ Wk1/paproblem.js
@@ -2,7 +2,6 @@ function additionMutator(arr, number) {
     let arr2 = [];
     for (let i = 0; i < arr.length; i++) {
         let num = arr[i];
-        // console.log(`this is iteration ${i}`, num, arr[i])
         num += number;
         console.log(`this is iteration ${i}`, num, arr[i])
         arr2.push(num)
@@ -14,7 +13,7 @@ function additionMutator(arr, number) {
 
 let nums1 = [3, 7, 1, 2];
 additionMutator(nums1, 4);
-console.log(nums1);     // [ 7, 11, 5, 6 ]
+console.log(additionMutator(nums1, 4));     // [ 7, 11, 5, 6 ]
 
 let nums2 = [11, 9, 4];
 additionMutator(nums2, -1);
