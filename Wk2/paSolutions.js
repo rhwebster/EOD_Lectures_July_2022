7ca0a65c04826c9b611914596d245600eca64fe8
Ryan Webster <ryan.h.webster@gmail.com>
2022-07-15 14:22:38 -0700

    pt2

---
+++ Wk2/paSolutions.js
@@ -0,0 +1,108 @@
+/***********************************************************************
+Write a function called `shoppingList(list)`. The shoppingList function will
+take in a starting list as an array and return a new function that will allow
+us to add to the list. When that return function is invoked with an
+argument, it will add the argument to the list and return the list.
+
+
+Look below to see how this function is invoked:
+
+const groceryList = ['eggs']
+const addToGroceryList = shoppingList(groceryList); // returns a function
+addToGroceryList('maple syrup'); // returns ['eggs', 'maple syrup']
+
+const clothesList = []
+const addToClothesList = shoppingList(clothesList);
+addToClothesList('tshirt'); // returns ['tshirt']
+addToClothesList('pants'); // returns ['tshirt', 'pants']
+console.log(clothesList); // ['tshirt', 'pants']
+
+***********************************************************************/
+
+function shoppingList(list) {
+    // Your code here
+    return (item) => {
+        list.push(item);
+        return list;
+    }
+}
+
+/***********************************************************************
+Write a function `pickyMyMap` that accepts an array and a callback as
+arguments. The function should call the callback for each element of the
+array, passing in the element and return a new array of the results of
+each call to the callback function. If the result of the callback function
+returns something falsey, then do not add it in the result array.
+
+const nums = [1, 2, 3, 4];
+pickyMyMap(nums, (num) => num - 2); // [-1, 1, 2]
+
+const booleans = [true, false, true, false, true];
+pickyMyMap(booleans, (boolean) => !boolean); // [true, true]
+
+You may not use Array's `map()`, `filter()`, or `forEach()` methods.
+***********************************************************************/
+
+function pickyMyMap(arr, cb) {
+    // Your code here
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i]
+        if (cb(el)) {
+            newArray.push(cb(el));
+        }
+    }
+    return newArray;
+}
+
+/***********************************************************************
+Write a function named `filterUserProfiles` that accepts an array of
+objects that represent user profiles and a callback function. You
+can expect the callback function to take in an object and return
+true or false.
+
+`filterUserProfiles` should use the callback function to return an
+array of names from the user profiles that, when passed into the
+callback function, return true. You can expect all user profiles to
+be passed in with a key of "name".
+
+const facebookUsers = [
+  { name: "John", age: "21", state: "Florida" },
+  { name: "Mary", age: "57", state: "California" },
+  { name: "Judy", age: "47", state: "Texas" },
+  { name: "Mike", age: "32", state: "New York" }
+];
+
+filterUserProfiles(facebookUsers, (user) => user.age > 30); // ["Mary", "Judy", "Mike"]
+filterUserProfiles(facebookUsers, (user) => user.state === "New York"); // ["Mike"]
+***********************************************************************/
+
+function filterUserProfiles(users, filter) {
+    // Your code here
+    let names = [];
+
+    users.forEach(obj => {
+        if (filter(obj)) {
+            names.push(obj.name);
+        }
+    });
+    return names;
+}
+
+/***********************************************************************
+Write a function using fat arrow syntax, `sentenceMaker` that
+takes in any number of arguments as strings, and create a sentence out
+of it. You may assume that all arguments will be strings. Add an exclamation
+mark at the end of the sentence.
+
+Examples:
+
+sentenceMaker('Hello', 'World'); // 'Hello World!'
+sentenceMaker('I', 'love', 'coding'); // 'I love coding!'
+sentenceMaker('The', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'); // 'The quick brown fox jumps over the lazy dog!'
+***********************************************************************/
+
+// Your code here
+const sentenceMaker = (...strings) => {
+    return strings.join(' ') + '!';
+};
