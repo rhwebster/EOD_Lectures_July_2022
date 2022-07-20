a3e2570cad44f0904dcc386b08caa7b253a9da67
Ryan Webster <ryan.h.webster@gmail.com>
2022-07-20 16:09:32 -0700

    isSorted

---
+++ Wk3/day3/isSorted.js
@@ -0,0 +1,16 @@
+/* Write a recursive solution called `isSorted` to determine if the input array 
+is sorted in ascending order.
+Examples:
+isSorted([1, 2, 3, 4, 5]); => true
+isSorted([1, 2, 4, 3, 5]); => false
+isSorted([2, 4, 6, 7, 8]); => true
+isSorted([5, 4, 3, 2, 1]); => false
+*/
+
+function isSorted(arr) {
+
+}
+
+// console.log(isSorted([1, 2, 3, 4, 5]));
+// console.log(isSorted([2, 4, 6, 8, 10]));
+// console.log(isSorted([5, 4, 3, 2, 1]));
